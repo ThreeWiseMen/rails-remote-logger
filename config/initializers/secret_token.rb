ce32f2e14ec2b4f8286cdf457250f3cdba1e29d8
Justin Wood <justin.k.wood@gmail.com>
2013-04-11 10:25:41 -0400

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRemoteLogger::Application.config.secret_token = '30ff7bec01740b52e65c91ffa754391bc12f894ec544738e53c6a38b6472b292e48fdbc39433b7d31575777da666a845e33037118487f8c9fb3d33391c233f3a'
