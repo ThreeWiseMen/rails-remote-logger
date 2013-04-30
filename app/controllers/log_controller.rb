00702c7883619524f7cd65e68144162717d06dfb
Justin Wood <justin.k.wood@gmail.com>
2013-04-30 09:11:32 -0400

    first requires a user to pick the domain they wish to look through

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,9 +1,13 @@
 class LogController < ApplicationController
   def index
-    @logs = Log.all
+    @logs = Log.all.uniq{ |l| l.host }
   end
 
   def search_by_token
-    @logs = Log.where(token: params[:token])
+    @logs = Log.find_all_by_token(params[:stoken])
+  end
+
+  def search_by_host
+    @logs = Log.find_all_by_host(params[:shost])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 RailsRemoteLogger::Application.routes.draw do
   root to: 'log#index'
 
-  match 'log/:token/search_by_token' => "log#search_by_token", as: :log_token_search
+  match 'log/:shost/search_by_host' => "log#search_by_host", as: :log_search_host
+  match 'log/:stoken/search_by_token' => "log#search_by_token", as: :log_search_token
 
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
